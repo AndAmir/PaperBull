c65295b01ee799d51fc07eb46e769f6b6d82a504
Parth <pvp53@njit.edu>
2021-04-16 01:28:20 +0000

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import logo from './logo.svg';
 import './App.css';
 import { Profile } from './';
@@ -7,32 +6,8 @@ function App() {
   return (
     <div className="App">
       <Profile />
-=======
-import './App.css';
-import io from 'socket.io-client';
-
-export const socket = io(); 
-
-function App() {
-  return (
-    <div>
->>>>>>> 795075bf9ab8cf44ffee3b3d8b7c6f13e2c24c9e
     </div>
-  );
+  )
 }
 
-// <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
 export default App;
