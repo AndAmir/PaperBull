cda3b8075498d4a5e5d4f7e58814cce46bd99c20
Perbhat <pk524@njit.edu>
2021-04-17 07:04:39 +0000

    added basic login and logout functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
+import React, { useState, useRef } from 'react';
 import './App.css';
 import io from 'socket.io-client';
 
-export const socket = io(); 
+export const socket = io();
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  const [thisUser, updateUser] = useState('');
+  const inputUser = useRef('');
+  
+  function onButtonClick() {
+    if(inputUser.current.value !== '') {
+      const user = inputUser.current.value;
+      updateUser( user )
+    }
+  }
+  
+  function logout() {
+    if(thisUser !== '') {
+      updateUser('');
+    }
+  }
+  
+    if(thisUser === '') {
+      
+      
+      return (
+        <div>
+          <input type="text" ref={inputUser} placeholder="username" required />
+          <button type="button" onClick={onButtonClick}>Log In</button>
+        </div>
+      );
+      
+      
+    }
+    else{
+      return (
+        <>
+        <h1>Username: {thisUser}</h1>
+        <button onClick = {logout}> Log Out </button>
+        </>
+        )
+    }
+    
+    
+
 }
 
 export default App;
