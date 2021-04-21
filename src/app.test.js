e21da7806d8833b73cc411d635adda755e760726
Perbhat <pk524@njit.edu>
2021-04-21 03:24:27 +0000

    added a procfile and jest tests

--- src/app.test.js
+++ src/app.test.js
@@ -8,10 +8,10 @@ test('Login button appears', () => {
   expect(loginButtonElement).toBeInTheDocument();
 });
 
-test('Buy and sell buttons show up', () => {
+test('Tests stocksearch flow', () => {
   const result = render(<StockSearch userID='perbhat'/>);
   const searchStock = screen.getByPlaceholderText('Enter Ticker Symbol...')
   fireEvent.change(searchStock, { target: { value: 'APPL' } });
   fireEvent.keyPress(searchStock, { key: 'Enter', code: 'Enter' })
-  
+  expect(searchStock).toBeInTheDocument()
 });
