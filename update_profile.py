fc46b903253f81c6274e000913b898afa44faf11
John Santelises <js2343@njit.edu>
2021-04-29 01:45:59 +0000

    Linting Unrelated Files :/

--- update_profile.py
+++ update_profile.py
@@ -6,11 +6,15 @@ def getUserStockDataFromDB(data, db):
 
     response = dict()
 
-    userName = data['userName']
+    userName = data["userName"]
     # get the users username_id
-    dbUserId = db.session.query(models.USERS).filter_by(username=userName).first().username_id
-    #get the stocks owned by that user to get their potential assets
-    stocksOwnedByUser = db.session.query(models.STOCKS).filter_by(username_id=dbUserId).all()
+    dbUserId = (
+        db.session.query(models.USERS).filter_by(username=userName).first().username_id
+    )
+    # get the stocks owned by that user to get their potential assets
+    stocksOwnedByUser = (
+        db.session.query(models.STOCKS).filter_by(username_id=dbUserId).all()
+    )
     # pass in tickerSymbol to helper function to get data !!!!PSA This might not be a list!!!!
     for tableData in stocksOwnedByUser:
         stock = tableData.ticker
@@ -21,23 +25,28 @@ def getUserStockDataFromDB(data, db):
         currentPrice = stock_transaction.helper_get_stock_price(stock)
         # DICT TO STORE DATA LIKE: {'quantity': 10, 'averagePrice': 32.08, 'currentPrice' : 60}
         dataRes = dict()
-        dataRes['quantity'] = quantity
-        dataRes['averagePrice'] = averagePrice
-        dataRes['currentPrice'] = currentPrice
-        #FINAL RETURN DICT TO STORE DATA LIKE
+        dataRes["quantity"] = quantity
+        dataRes["averagePrice"] = averagePrice
+        dataRes["currentPrice"] = currentPrice
+        # FINAL RETURN DICT TO STORE DATA LIKE
         response[stock] = dataRes
 
     return response
 
+
 def getCashBalance(data, db):
     response = dict()
-    
-    userName = data['userName']
-    
-    userId = db.session.query(models.USERS).filter_by(username=userName).first().username_id
-    cashBalance = db.session.query(models.USERS).filter_by(username=userName).first().cash_balance
-    
-    response['cashBalance'] = cashBalance
-    response['userId'] = userId
-    
+
+    userName = data["userName"]
+
+    userId = (
+        db.session.query(models.USERS).filter_by(username=userName).first().username_id
+    )
+    cashBalance = (
+        db.session.query(models.USERS).filter_by(username=userName).first().cash_balance
+    )
+
+    response["cashBalance"] = cashBalance
+    response["userId"] = userId
+
     return response
