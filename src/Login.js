dcad74d965d9910855ab3f3187005f389ad48e5b
John Santelises <js2343@njit.edu>
2021-05-04 04:13:29 +0000

    Experimental Changes

--- src/Login.js
+++ src/Login.js
@@ -3,10 +3,7 @@ import PropTypes from 'prop-types';
 import { GoogleLogin } from 'react-google-login';
 import { socket } from './App';// eslint-disable-line
 
-const { NODE_ENV } = process.env;
-const clientID = NODE_ENV === 'production' ? window.API_URL : process.env.REACT_APP_GOOGLE_CLIENT;
-console.log(`NODE_ENV = ${NODE_ENV}`, window.API_URL, process.env.REACT_APP_GOOGLE_CLIENT);
-console.log(`ClientID:${clientID}`);
+const clientID = process.env.REACT_APP_GOOGLE_CLIENT;
 
 export function Login({ updateUser, updateName, setImageURL }) {
   const onSuccess = (res) => {
